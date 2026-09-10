43ca04ca5ddfd6b497fba3ad7db6319a75438144
Brayan <lsouzadiasvaz@gmail.com>
2026-09-10 11:19:47 -0300

    feat: add tests

--- src/config/settings.py
+++ src/config/settings.py
@@ -18,7 +18,7 @@ class Environment:
 
         self.envs = ["redis_port", "redis_host", "url", "sing", 
                      "origin", "rate_limit", "global_rate_limit", 
-                     "redis_password", "email_user", "email_password"]
+                     "redis_password", "email_user", "password_user"]
 
 
 
